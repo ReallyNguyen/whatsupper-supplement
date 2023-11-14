29199f54595cfebe6c567a50b3cc6b154ac3fefd
EricQian01 <ericqian1203@gmail.com>
2023-11-13 16:16:18 -0800

    Animated Slides Push

---
+++ components/Animation/GeneratorRefresh.js
@@ -0,0 +1,20 @@
+import { useLottie } from "lottie-react";
+import refreshAnimation from "@/assets/Generator-Refresh.json";
+
+const style = {
+    height: 400,
+};
+
+const Refresh = () => {
+    const options = {
+        animationData: refreshAnimation,
+        loop: true,
+        autoplay: true,
+    };
+
+    const { View } = useLottie(options, style);
+
+    return View;
+};
+
+export default Refresh;

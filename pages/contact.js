b89d495e060de92eec6aed5e01632f42d2ee1a86
ReallyNguyen <jnguyen188@my.bcit.ca>
2023-12-07 00:19:17 -0800

    contact and nav responsive

--- pages/contact.js
+++ pages/contact.js
@@ -8,12 +8,12 @@ export default function Contact() {
     return (
         <ThemeProvider>
             <div className={`${styles.main}`}>
-                    <NavBar />
-                    <div className={`${styles.landingContent}`}>
-                        <p className={`${styles.header}`}>Interested in learning more?</p>
-                        <p className={`${styles.subheader}`}>We’d love to hear from you!</p>
-                        <ContactForm />
-                    </div>
+                <NavBar />
+                <div className={`${styles.landingContent}`}>
+                    <p className={`${styles.header}`}>Interested in learning more?</p>
+                    <p className={`${styles.subheader}`}>We’d love to hear from you!</p>
+                    <ContactForm />
+                </div>
             </div>
         </ThemeProvider>
     )
